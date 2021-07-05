d1f9d4b62c5d16670bf7010018a6a66bc430e675
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-07-04 22:28:52 -0500

    Chapter 8 Ex 03 Finished

--- Chapter9ex03.c
+++ Chapter9ex03.c
@@ -1,31 +1,32 @@
-#include<stdio.h>
-char prueba[1000];
-char prueba2[1000];
-int main() 
+/********************************************************
+ * Ex03 -- El mayor. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: Usando una funcion, saber cuantas veces aparece un numero en un array. *
+ * *
+ * Usage: *
+ * mete los valores y el numero a conocer para poder visualizarlo*
+ * Restrictions* Solamente puede ser ejecutado, no podras registrar las variables que tu necesitas a menos que lo hagas en el archivo central
+ ********************************************************/
+#include <stdio.h>
+
+int i(int can, int *array, int md); //defino las variables usadas
+
+int main()
 {
-    int m=0,a;
-    int xd; //declaro las variables a usar
-    printf("dame tamaño de array\n");
-    fgets(prueba,sizeof(prueba),stdin);
-    sscanf(prueba,"%d",&xd); //pido el tamaño del array a usar
-    int v[xd]; //meto la variable dentro del lugar 
-    printf("dame los numeros del array\n");
-    for(int i=0;i<xd;i++){
-      fgets(prueba,sizeof(prueba),stdin);
-      sscanf(prueba,"%d",&v[i]);
-      //escaneo en un for cada variable definida y las guardo en el array
-    }
-    printf("dame el numero que quieres saber cuantas veces aparece en el arreglo\n");
-    fgets(prueba2,sizeof(prueba2),stdin);
-    sscanf(prueba2,"%d",&m); 
-    m = v[0];  //voy a guardar mi variable en esa condicion m
-    for(int i = 1; i < xd; ++i) //nuevamente hago el escaneo variable por variable
-    {
-        if(v[i] == m) //si es igual al seleccionado
-        {
-            m=m+1; //se hace una suma si este es igual para hacer un conteo
-        }
-    }
-    printf("el numero seleccionado se repite %d veces\n", m); //imprimo resultado
-    return 0;
+	int array[100] = {10,5,8,7,9,6,3,4,1,7,8,9,2,5,4,7,8,5,4,1,2,3,5,4,7,8,9,6,3,4,7,8,9}; //meto los numeros en el array asi como el valor que quiero conocer cuantas veces se repite
+	int can = 8;
+
+	printf("Array contains %d times the number %d\n", i(can, array, 100), can); //expreso cuantoas veces aparece el numero seleccionado
+
+	return 0;
+}
+int i(int can, int *array, int md) //los valores que tengo arriba, los traigo abajo
+{
+	if (md == 0) //si este valor vale 0, se regresa a la funcion inicial
+		return 0;
+
+	return (*array == can) + i(can, array+1, md-1); //se opera y se busca ccual es el numero que mas veces se repite
 }
